c92f070676fe6c7409309d693a32308a21c5c725
Thomas-VWD <thomas.verwaerde@gmail.com>
2023-04-04 12:14:45 +0200

    pokemonList[] + PokemonCard() if / else

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,34 @@
-function PokemonCard() {
-    return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Un Bulbizarre" />
-            <figcaption>Bulbizarre</figcaption>
-        </figure>
-    )
-}
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 
+function PokemonCard() {
+    const pokemons = pokemonList.map((pokemon) => {
+        if (!pokemon.imgSrc) {
+            return (
+                <figure>
+                    <p>???</p>
+                    <figcaption>{pokemon.name}</figcaption>
+                </figure>
+            );
+        }
+        else {
+            return (
+                <figure>
+                    <img src={pokemon.imgSrc} />
+                    <figcaption>{pokemon.name}</figcaption>
+                </figure>
+            );
+        }
+    }
+    );
+    return <div>{pokemons}</div>
+}
 export default PokemonCard;
