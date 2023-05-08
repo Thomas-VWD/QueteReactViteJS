b6073a33044c5503b0c4a54b28fa2b2a1bd92fa7
Thomas-VWD <thomas.verwaerde@gmail.com>
2023-05-08 10:18:33 +0200

    simple carte css dans le composant jsx

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,21 @@
 import PropTypes from "prop-types";
 
+const card = {
+    width: "200px",
+    padding: "1rem",
+    backgroundColor: "lightsalmon",
+    borderRadius: "5px",
+    boxShadow: "0px 0px 5px gray",
+    color: "white",
+    fontSize: "1.6rem",
+    textAlign: "center",
+    textShadow: "0px 0px 5px gray",
+}
+
 function PokemonCard({ pokemon }) {
     if (!pokemon.imgSrc) {
         return (
-            <figure className="card">
+            <figure style={card}>
                 <p>???</p>
                 <figcaption>{pokemon.name}</figcaption>
             </figure>
@@ -11,7 +23,7 @@ function PokemonCard({ pokemon }) {
     }
     else {
         return (
-            <figure className="card">
+            <figure style={card}>
                 <img src={pokemon.imgSrc} className="card-img"/>
                 <figcaption>{pokemon.name}</figcaption>
             </figure>
